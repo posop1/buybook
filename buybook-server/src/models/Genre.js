903b7a1d3c355b3a22a8cb9913808dbd73129bf6
posop <artur.yakhin52@gmail.com>
2023-02-18 15:20:32 +0500

    delete first version

--- buybook-server/src/models/Genre.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const GenreSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  books: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }]
-})
-
-export default mongoose.model('Genre', GenreSchema)
--- buybook-server/src/routes/comments.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { createComment } from '../controllers/comments.js'
-import { checkAuth } from '../utils/checkAuth.js'
-
-const router = new Router()
-
-router.post('/:id', checkAuth, createComment)
-
-export default router
